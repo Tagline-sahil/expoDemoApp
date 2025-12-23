e6b58621a35c5195a07dd7713f044ef45330200b
Tagline-Sahil <sbr@taglineinfotech.com>
2025-12-23 11:06:20 +0530

    feat: Migrate from app.json to app.config.js to support environment-specific configurations for app name, bundle ID, and package.

---
+++ app.config.js
@@ -0,0 +1,66 @@
+export default ({ config }) => {
+  const env = process.env.EXPO_PUBLIC_APP_ENV;
+
+  const isDev = env === "development";
+  const isStaging = env === "staging";
+
+  return {
+    ...config,
+
+    name: isDev
+      ? "expoDemoApp Dev"
+      : isStaging
+      ? "expoDemoApp Staging"
+      : "expoDemoApp",
+
+    slug: "expoDemoApp",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: isDev
+        ? "com.expoDemoApp.dev"
+        : isStaging
+        ? "com.expoDemoApp.staging"
+        : "com.expoDemoApp",
+    },
+
+    android: {
+      package: isDev
+        ? "com.expoDemoApp.dev"
+        : isStaging
+        ? "com.expoDemoApp.staging"
+        : "com.expoDemoApp",
+
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+
+      edgeToEdgeEnabled: true,
+    },
+
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+
+    extra: {
+      eas: {
+        projectId: "c2c9931f-436c-4933-8ef9-901e4b54441c",
+      },
+      env,
+    },
+
+    owner: "tagline_sahil",
+  };
+};
